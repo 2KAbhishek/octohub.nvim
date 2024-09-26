20cc76d835de48dad423ed5be73d4e0b2661b4bb
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-09-26 17:35:38 +0530

    feat: remove stats code

--- plugin/octorepos.lua
+++ plugin/octorepos.lua
@@ -1,13 +1,5 @@
 local repos = require('octorepos')
 
-vim.api.nvim_create_user_command('GitHubStats', function(opts)
-    repos.show_github_stats(opts.args)
-end, { nargs = '?' })
-
-vim.api.nvim_create_user_command('GitHubProfile', function(opts)
-    repos.open_github_profile(opts.args)
-end, { nargs = '?' })
-
 vim.api.nvim_create_user_command('Repos', function(opts)
     repos.show_repos(opts.args)
 end, { nargs = '?' })
