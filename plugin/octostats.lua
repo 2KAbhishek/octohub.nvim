3e5bde7e7da4284889335db9d315213b8117fdbf
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-09-26 19:17:58 +0530

    feat: update command names

--- plugin/octostats.lua
+++ plugin/octostats.lua
@@ -1,9 +1,9 @@
 local stats = require('octostats')
 
-vim.api.nvim_create_user_command('GitHubStats', function(opts)
+vim.api.nvim_create_user_command('OctoStats', function(opts)
     stats.show_github_stats(opts.args)
 end, { nargs = '?' })
 
-vim.api.nvim_create_user_command('GitHubProfile', function(opts)
+vim.api.nvim_create_user_command('OctoProfile', function(opts)
     stats.open_github_profile(opts.args)
 end, { nargs = '?' })
