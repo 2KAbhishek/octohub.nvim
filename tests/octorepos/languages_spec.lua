119fe1ba9e14ea1acf803c7a5a5db0ac1d4d0369
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-10-06 11:29:45 +0530

    test: add language spec

---
+++ tests/octorepos/languages_spec.lua
@@ -0,0 +1,26 @@
+local languages = require('octorepos.languages')
+
+describe('language_to_filetype', function()
+    it('works with known languages', function()
+        assert(languages.language_to_filetype('Python') == 'py', 'language_to_filetype function with param = Python')
+        assert(
+            languages.language_to_filetype('JavaScript') == 'js',
+            'language_to_filetype function with param = JavaScript'
+        )
+        assert(
+            languages.language_to_filetype('Markdown') == 'md',
+            'language_to_filetype function with param = Markdown'
+        )
+    end)
+
+    it('works with unknown language', function()
+        assert(
+            languages.language_to_filetype('NonExistentLanguage') == 'nonexistentlanguage',
+            'language_to_filetype function with param = NonExistentLanguage'
+        )
+    end)
+
+    it('works with nil language', function()
+        assert(languages.language_to_filetype(vim.NIL) == 'md', 'language_to_filetype function with param = nil')
+    end)
+end)
