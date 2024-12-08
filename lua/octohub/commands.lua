366cf36cbafe07e4793ef85ce21d728711fa1e43
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-12-09 02:49:30 +0530

    feat: add more commands

--- lua/octohub/commands.lua
+++ lua/octohub/commands.lua
@@ -6,6 +6,12 @@ local config = require('octohub.config').config
 local M = {}
 
 M.setup = function()
+    local function add_octorepos_command(name, sort_arg, type_arg)
+        vim.api.nvim_create_user_command(name, function(_)
+            repos.show_repos('', sort_arg, type_arg)
+        end, { nargs = '?' })
+    end
+
     vim.api.nvim_create_user_command('OctoRepos', function(opts)
         local user_arg, sort_arg, type_arg = '', '', ''
 
@@ -21,6 +27,23 @@ M.setup = function()
 
         repos.show_repos(user_arg, sort_arg, type_arg)
     end, { nargs = '*' })
+
+    add_octorepos_command('OctoReposByCreated', 'create', '')
+    add_octorepos_command('OctoReposByForks', 'fork', '')
+    add_octorepos_command('OctoReposByIssues', 'issue', '')
+    add_octorepos_command('OctoReposByLanguage', 'language', '')
+    add_octorepos_command('OctoReposByName', 'name', '')
+    add_octorepos_command('OctoReposByPushed', 'push', '')
+    add_octorepos_command('OctoReposBySize', 'size', '')
+    add_octorepos_command('OctoReposByStars', 'star', '')
+    add_octorepos_command('OctoReposByUpdated', 'update', '')
+    add_octorepos_command('OctoReposByWatchers', 'watch', '')
+
+    add_octorepos_command('OctoReposTypeArchived', '', 'archive')
+    add_octorepos_command('OctoReposTypeForked', '', 'fork')
+    add_octorepos_command('OctoReposTypePrivate', '', 'private')
+    add_octorepos_command('OctoReposTypeStarred', '', 'star')
+    add_octorepos_command('OctoReposTypeTemplate', '', 'template')
 
     vim.api.nvim_create_user_command('OctoRepo', function(opts)
         local args = vim.split(opts.args, ' ')
@@ -71,17 +94,28 @@ M.setup = function()
         end
 
         add_keymap('<leader>goo', ':OctoRepos<CR>', 'All Repos')
-        add_keymap('<leader>gos', ':OctoRepos sort:stars<CR>', 'Repos by Stars')
-        add_keymap('<leader>goS', ':OctoRepos type:star<CR>', 'Starred Repos')
-        add_keymap('<leader>goi', ':OctoRepos sort:issues<CR>', 'Repos by Issues')
-        add_keymap('<leader>gou', ':OctoRepos sort:updated<CR>', 'Repos by Last Updated')
-        add_keymap('<leader>gop', ':OctoRepos type:private<CR>', 'Private Repos')
-        add_keymap('<leader>gof', ':OctoRepos type:fork<CR>', 'Forked Repos')
-        add_keymap('<leader>goc', ':OctoRepo<CR>', 'Open Repo')
-        add_keymap('<leader>got', ':OctoStats<CR>', 'All Stats')
+
+        add_keymap('<leader>gob', ':OctoReposBySize<CR>', 'Repos by Size')
+        add_keymap('<leader>goc', ':OctoReposByCreated<CR>', 'Repos by Created')
+        add_keymap('<leader>gof', ':OctoReposByForks<CR>', 'Repos by Forks')
+        add_keymap('<leader>goi', ':OctoReposByIssues<CR>', 'Repos by Issues')
+        add_keymap('<leader>gol', ':OctoReposByLanguage<CR>', 'Repos by Language')
+        add_keymap('<leader>gop', ':OctoReposByPushed<CR>', 'Repos by Pushed')
+        add_keymap('<leader>gos', ':OctoReposByStars<CR>', 'Repos by Stars')
+        add_keymap('<leader>gou', ':OctoReposByUpdated<CR>', 'Repos by Updated')
+        add_keymap('<leader>goW', ':OctoReposByWatchers<CR>', 'Repos by Watchers')
+
+        add_keymap('<leader>goA', ':OctoReposTypeArchived<CR>', 'Archived Repos')
+        add_keymap('<leader>goF', ':OctoReposTypeForked<CR>', 'Forked Repos')
+        add_keymap('<leader>goP', ':OctoReposTypePrivate<CR>', 'Private Repos')
+        add_keymap('<leader>goS', ':OctoReposTypeStarred<CR>', 'Starred Repos')
+        add_keymap('<leader>goT', ':OctoReposTypeTemplate<CR>', 'Template Repos')
+
         add_keymap('<leader>goa', ':OctoActivityStats<CR>', 'Activity Stats')
         add_keymap('<leader>gog', ':OctoContributionStats<CR>', 'Contribution Graph')
         add_keymap('<leader>gor', ':OctoRepoStats<CR>', 'Repo Stats')
+        add_keymap('<leader>got', ':OctoStats<CR>', 'All Stats')
+
         add_keymap('<leader>goh', ':OctoProfile<CR>', 'Open GitHub Profile')
         add_keymap('<leader>gow', ':OctoRepoWeb<CR>', 'Open Repo in Browser')
     end
