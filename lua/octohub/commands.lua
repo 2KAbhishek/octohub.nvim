85812c784f2a816cf2acae65c9b7bab4ce9b77e3
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-23 10:48:12 +0530

    refactor: make commands more readable

--- lua/octohub/commands.lua
+++ lua/octohub/commands.lua
@@ -4,6 +4,7 @@ local web = require('octohub.web')
 local config = require('octohub.config').config
 local legacy = require('octohub.legacy')
 
+---@class OctohubCommands
 local M = {}
 
 ---Add all default keymaps for Octohub commands
@@ -38,43 +39,76 @@ local function add_default_keymaps()
     add_keymap('<leader>gow', ':Octohub web repo<CR>', 'Open Repo in Browser')
 end
 
----Completion for Octohub subcommands
+---Parse command line arguments, removing empty strings
+---@param cmdline string
 ---@return string[]
-local function complete_repos_subcommands()
-    return { 'repos', 'repo', 'stats', 'web' }
+local function parse_args(cmdline)
+    local args = vim.split(cmdline, ' ')
+    return vim.tbl_filter(function(arg)
+        return arg ~= ''
+    end, args)
 end
 
----Completion for repos sort/type params
+---Filter items by prefix match
+---@param items string[]
+---@param prefix string
 ---@return string[]
-local function complete_repos_params()
-    return {
-        'sort:created',
-        'sort:forks',
-        'sort:issues',
-        'sort:language',
-        'sort:name',
-        'sort:pushed',
-        'sort:size',
-        'sort:stars',
-        'sort:updated',
-        'type:archived',
-        'type:forked',
-        'type:private',
-        'type:starred',
-        'type:template',
+local function filter_by_prefix(items, prefix)
+    return vim.tbl_filter(function(item)
+        return item:sub(1, #prefix) == prefix
+    end, items)
+end
+
+---Get completion options for different contexts
+---@param context string
+---@return string[]
+local function get_completion_options(context)
+    local options = {
+        subcommands = { 'repos', 'repo', 'stats', 'web' },
+        repos_params = {
+            'sort:created',
+            'sort:forks',
+            'sort:issues',
+            'sort:language',
+            'sort:name',
+            'sort:pushed',
+            'sort:size',
+            'sort:stars',
+            'sort:updated',
+            'type:archived',
+            'type:forked',
+            'type:private',
+            'type:starred',
+            'type:template',
+        },
+        stats_subcommands = { 'activity', 'contributions', 'repo' },
+        web_subcommands = { 'profile', 'repo' },
     }
+
+    return options[context] or {}
 end
 
----Completion for stats subcommands
----@return string[]
-local function complete_stats_subcommands()
-    return { 'activity', 'contributions', 'repo' }
-end
+---Parse parameters with prefixes (e.g., "sort:", "type:", "count:")
+---@param args string[]
+---@param start_idx integer
+---@return table<string, string>
+local function parse_prefixed_params(args, start_idx)
+    local params = {}
+    local user_arg = ''
 
----Completion for web subcommands
----@return string[]
-local function complete_web_subcommands()
-    return { 'profile', 'repo' }
+    for i = start_idx, #args do
+        local arg = args[i]
+        local prefix, value = arg:match('^(%w+):(.+)$')
+
+        if prefix and value then
+            params[prefix] = value
+        else
+            user_arg = arg
+        end
+    end
+
+    params.user = user_arg
+    return params
 end
 
 ---Main completion function for Octohub command
@@ -83,124 +117,124 @@ end
 ---@param cursorpos integer
 ---@return string[]
 local function complete_octohub(arglead, cmdline, cursorpos)
-    local args = vim.split(cmdline, ' ')
+    local args = parse_args(cmdline)
     local arg_count = #args
 
-    args = vim.tbl_filter(function(arg)
-        return arg ~= ''
-    end, args)
-
     if arg_count == 2 then
-        return vim.tbl_filter(function(cmd)
-            return cmd:sub(1, #arglead) == arglead
-        end, complete_repos_subcommands())
+        return filter_by_prefix(get_completion_options('subcommands'), arglead)
     elseif arg_count > 2 then
         local subcommand = args[2]
+
         if subcommand == 'repos' then
-            return vim.tbl_filter(function(param)
-                return param:sub(1, #arglead) == arglead
-            end, complete_repos_params())
+            return filter_by_prefix(get_completion_options('repos_params'), arglead)
         elseif subcommand == 'stats' and arg_count == 3 then
-            return vim.tbl_filter(function(cmd)
-                return cmd:sub(1, #arglead) == arglead
-            end, complete_stats_subcommands())
+            return filter_by_prefix(get_completion_options('stats_subcommands'), arglead)
         elseif subcommand == 'web' and arg_count == 3 then
-            return vim.tbl_filter(function(cmd)
-                return cmd:sub(1, #arglead) == arglead
-            end, complete_web_subcommands())
+            return filter_by_prefix(get_completion_options('web_subcommands'), arglead)
         end
     end
 
     return {}
 end
 
+---Handle repos subcommand
+---@param args string[]
+local function handle_repos_command(args)
+    local params = parse_prefixed_params(args, 2)
+    repos.show_repos(params.user or '', params.sort or config.repos.sort_by, params.type or config.repos.repo_type)
+end
+
+---Handle repo subcommand
+---@param args string[]
+local function handle_repo_command(args)
+    if #args == 2 then
+        repos.open_repo(args[2])
+    elseif #args == 3 then
+        repos.open_repo(args[3], args[2])
+    else
+        print('Usage: Octohub repo <name> [user]')
+    end
+end
+
+---Handle stats subcommand
+---@param args string[]
+local function handle_stats_command(args)
+    if #args == 1 then
+        stats.show_all_stats('')
+    elseif #args == 2 then
+        local subcommand = args[2]
+        if subcommand == 'activity' then
+            stats.show_activity_stats('', nil)
+        elseif subcommand == 'contributions' then
+            stats.show_contribution_stats('')
+        elseif subcommand == 'repo' then
+            stats.show_repo_stats('')
+        else
+            stats.show_all_stats(args[2])
+        end
+    elseif #args >= 3 then
+        local subcommand = args[2]
+        if subcommand == 'activity' then
+            local params = parse_prefixed_params(args, 3)
+            stats.show_activity_stats(params.user or '', tonumber(params.count))
+        elseif subcommand == 'contributions' then
+            stats.show_contribution_stats(args[3])
+        elseif subcommand == 'repo' then
+            stats.show_repo_stats(args[3])
+        end
+    end
+end
+
+---Handle web subcommand
+---@param args string[]
+local function handle_web_command(args)
+    if #args == 1 then
+        print('Usage: Octohub web <profile|repo> [user]')
+    elseif args[2] == 'profile' then
+        local user = #args > 2 and args[3] or ''
+        web.open_github_profile(user)
+    elseif args[2] == 'repo' then
+        web.open_repo_web()
+    else
+        print('Usage: Octohub web <profile|repo> [user]')
+    end
+end
+
+---Main command handler
+---@param opts table
+local function octohub_command(opts)
+    local args = parse_args(opts.args)
+
+    if #args == 0 then
+        repos.show_repos('', config.repos.sort_by, config.repos.repo_type)
+        return
+    end
+
+    local subcommand = args[1]
+    local handlers = {
+        repos = handle_repos_command,
+        repo = handle_repo_command,
+        stats = handle_stats_command,
+        web = handle_web_command,
+    }
+
+    local handler = handlers[subcommand]
+    if handler then
+        handler(args)
+    else
+        print('Unknown subcommand: ' .. subcommand)
+        print('Usage: Octohub <subcommand> [options]')
+        print('Available subcommands: repos, repo, stats, web')
+    end
+end
+
 ---Add the main Octohub command
 local function add_octohub_command()
-    vim.api.nvim_create_user_command('Octohub', function(opts)
-        local args = vim.split(opts.args, ' ')
-        args = vim.tbl_filter(function(arg)
-            return arg ~= ''
-        end, args)
-
-        if #args == 0 then
-            repos.show_repos('', config.repos.sort_by, config.repos.repo_type)
-            return
-        end
-
-        local subcommand = args[1]
-
-        if subcommand == 'repos' then
-            local user_arg, sort_arg, type_arg = '', '', ''
-
-            for i = 2, #args do
-                local arg = args[i]
-                if arg:match('^sort:') then
-                    sort_arg = arg:sub(6)
-                elseif arg:match('^type:') then
-                    type_arg = arg:sub(6)
-                else
-                    user_arg = arg
-                end
-            end
-
-            repos.show_repos(user_arg, sort_arg, type_arg)
-        elseif subcommand == 'repo' then
-            if #args == 2 then
-                repos.open_repo(args[2])
-            elseif #args == 3 then
-                repos.open_repo(args[3], args[2])
-            else
-                print('Usage: Octohub repo <name> [user]')
-            end
-        elseif subcommand == 'stats' then
-            if #args == 1 then
-                stats.show_all_stats('')
-            elseif #args == 2 then
-                if args[2] == 'activity' then
-                    stats.show_activity_stats('', nil)
-                elseif args[2] == 'contributions' then
-                    stats.show_contribution_stats('')
-                elseif args[2] == 'repo' then
-                    stats.show_repo_stats('')
-                else
-                    stats.show_all_stats(args[2])
-                end
-            elseif #args >= 3 then
-                local stats_subcommand = args[2]
-                if stats_subcommand == 'activity' then
-                    local user_arg, count_arg = '', ''
-                    for i = 3, #args do
-                        local arg = args[i]
-                        if arg:match('^count:') then
-                            count_arg = arg:sub(7)
-                        else
-                            user_arg = arg
-                        end
-                    end
-                    stats.show_activity_stats(user_arg, tonumber(count_arg))
-                elseif stats_subcommand == 'contributions' then
-                    stats.show_contribution_stats(args[3])
-                elseif stats_subcommand == 'repo' then
-                    stats.show_repo_stats(args[3])
-                end
-            end
-        elseif subcommand == 'web' then
-            if #args == 1 then
-                print('Usage: Octohub web <profile|repo> [user]')
-            elseif args[2] == 'profile' then
-                local user = #args > 2 and args[3] or ''
-                web.open_github_profile(user)
-            elseif args[2] == 'repo' then
-                web.open_repo_web()
-            else
-                print('Usage: Octohub web <profile|repo> [user]')
-            end
-        else
-            print('Unknown subcommand: ' .. subcommand)
-            print('Usage: Octohub <subcommand> [options]')
-            print('Available subcommands: repos, repo, stats, web')
-        end
-    end, { nargs = '*', complete = complete_octohub })
+    vim.api.nvim_create_user_command('Octohub', octohub_command, {
+        nargs = '*',
+        complete = complete_octohub,
+        desc = 'GitHub integration commands',
+    })
 end
 
 ---Setup Octohub commands and keymaps
